263bd21f4011a85a670b0f5e9ff4d25f39eb03f8
jaygittings <jaygittings@yahoo.com>
2020-05-03 13:33:17 -0400

    Initial commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int delay = 3;
+
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadScene(currentSceneIndex + 1, delay));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LoadScene(int index, int delay)
+    {
+        yield return new WaitForSeconds(delay);
+        LoadScene(index);
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] AudioClip loadingSound;
+    [SerializeField] float volume = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(loadingSound, Camera.main.transform.position, volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
