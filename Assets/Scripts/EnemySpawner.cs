1932bf8749fba4bd9888e3f5620fc97b44e09dc4
jaygittings <jaygittings@yahoo.com>
2020-05-04 17:31:34 -0400

    Feature Updates
    Add 1st Enemy sprite, trophy and enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 2f;
+    [SerializeField] List<GameObject> enemies = null;
+
+
+    bool shouldSpawn = true;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while(shouldSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            Instantiate(enemies[0], transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
